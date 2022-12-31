c0189db9112485edb599c80fa3408d7f2c64b6ff
chrisdanielwise <chrisdanielwise@gmail.com>
2022-12-31 20:58:52 +0100

    made changes

---
+++ Script/main.js
@@ -0,0 +1,8 @@
+let pickupToggle1 = document.getElementById('pickup-toggle1');
+let pickupToggle2 = document.getElementById('pickup-toggle2');
+let pickupToggle3 = document.getElementById('pickup-toggle3');
+let pickupIcon = document.querySelector('[pickup-list2 li]');
+
+pickupIcon.addEventListener('load',function(){
+    pickupIcon.style.backgroundColor = 'red'
+})
